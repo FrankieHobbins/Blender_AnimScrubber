01ea6304df04b4c2043734f31241e3ef3ea681ee
FrankieHobbins <acepainkiller@gmail.com>
2016-04-20 08:53:30 +0100

    Latest version
    added sensitivity settings in the hotkey setup and a tickbox and settings to scale based on scene length

--- animscrubberkey.py
+++ animscrubberkey.py
@@ -3,7 +3,7 @@ import bpy
 bl_info = {
 "name": "F Scrub Timeline",
 "author": "Frankie",
-"version": (0,1),
+"version": (0,2),
 "blender": (2, 77, 0),
 "location": "3d view",
 "description": "press ALT left click and move mouse in 3d view to change time. Will loop at end frame",
@@ -23,12 +23,14 @@ class ScrubTimelineKey(bpy.types.Operator):
 
     def __del__(self):
         print("End")
-
-#   def execute(self, context):
-#       return {'FINISHED'}
-
+    
+    Scale = bpy.props.FloatProperty(name="Sensitivity Scale", default=1)
+    ScaleBySceneLength = bpy.props.BoolProperty(name="Adjust Sensitivity With Scene Length", default=False)
+    StandardSceneLength = bpy.props.FloatProperty(name="Standard Scene Length", default=100)
+    LengthScale = bpy.props.FloatProperty(name="Length Scale", default=1)
+    
     def modal(self, context, event):
-        self.valued = event.mouse_x / 10
+        self.startingvalue = event.mouse_x / 10 * self.Scale * ((((bpy.context.scene.frame_end / self.StandardSceneLength)*self.LengthScale) * self.ScaleBySceneLength) + 1)
         if event.type == 'MOUSEMOVE':  #move frames
             if bpy.context.scene.frame_current > bpy.context.scene.frame_end:
                 bpy.context.scene.frame_set(bpy.context.scene.frame_start)
@@ -37,42 +39,29 @@ class ScrubTimelineKey(bpy.types.Operator):
                 bpy.context.scene.frame_set(bpy.context.scene.frame_end)
                 self.looped -= 1
             else:
-                bpy.context.scene.frame_set(self.valued - self.dampedvalue + self.sframe - (bpy.context.scene.frame_end*self.looped))
+                bpy.context.scene.frame_set(self.startingvalue - self.dampedvalue + self.sframe - (bpy.context.scene.frame_end*self.looped))
         elif event.type == 'LEFTMOUSE' and event.value == "RELEASE":  # Confirm
             return {'FINISHED'}
         return {'RUNNING_MODAL'}
 
     def invoke(self, context, event):
         self.sframe = bpy.context.scene.frame_current 
-        self.value = event.mouse_x 
-        self.dampedvalue = event.mouse_x / 10
+        self.dampedvalue = event.mouse_x / 10 * self.Scale * ((((bpy.context.scene.frame_end / self.StandardSceneLength)*self.LengthScale) * self.ScaleBySceneLength) + 1)
         self.looped = 0
-        self.a = bpy.context.object.name 
-
+    
         context.window_manager.modal_handler_add(self)
         return {'RUNNING_MODAL'}
 
-# store keymaps here to access after registration
-addon_fkeymaps = []
-            
 def register():
     bpy.utils.register_class(ScrubTimelineKey)
-        
-    # handle the keymap
+    
     wm = bpy.context.window_manager
     km = wm.keyconfigs.addon.keymaps.new(name='3D View', space_type='VIEW_3D', region_type='WINDOW', modal=False)
-    kmi = km.keymap_items.new('anim.scrubberkey', 'LEFTMOUSE', 'PRESS', alt=True)  
-    addon_fkeymaps.append(km)
-
+    kmi = km.keymap_items.new('anim.scrubberkey', 'LEFTMOUSE', 'PRESS', alt=True)     
 
 def unregister():
     bpy.utils.unregister_class(ScrubTimelineKey)
-    # handle the keymap
-    wm = bpy.context.window_manager
-    for km in addon_fkeymaps:
-        wm.keyconfigs.addon.keymaps.remove(km)
-    # clear the list
-    del addon_fkeymaps[:]
+    
     
 # This allows you to run the script directly from blenders text editor
 # to test the addon without having to install it.
